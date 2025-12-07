981afeb945d5bb6508f6e1aa2d41ecf223d0f14a
AlienBlox <alienthebest123@outlook.com>
2025-12-07 12:31:33 +0000

    wee

--- AlienBloxUtility.cs
+++ AlienBloxUtility.cs
@@ -13,18 +13,22 @@ namespace AlienBloxUtility
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class AlienBloxUtility : Mod
 	{
+        private static readonly object fileAccessLock = new object();
+
         public override void Load()
         {
             try
             {
-                DirectoryInfo Dir = Directory.CreateDirectory(Main.SavePath + "\\AlienBloxUtility\\Cache");
+                DirectoryInfo Dir = Directory.CreateDirectory(Path.Combine(Main.SavePath, "AlienBloxUtility", "Cache"));
 
-                lock (Dir)
+                if (Dir.Exists)
                 {
-                    if (Dir.Exists)
+                    // Only lock when performing the critical file operation
+                    lock (fileAccessLock)
                     {
                         string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "tModUnpacker.exe");
 
+                        // ExtractExe should only be called inside the lock if it's performing file I/O operations
                         InitialiseUtilities.ExtractExe("AlienBloxTools.Utilities.IncludedExes.tModUnpacker.exe", Dir.FullName);
                     }
                 }
