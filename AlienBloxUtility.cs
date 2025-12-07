ccb2aa5ec8b8df296b3625b4e22e66671980493d
AlienBlox <alienthebest123@outlook.com>
2025-12-07 12:34:57 +0000

    test

--- AlienBloxUtility.cs
+++ AlienBloxUtility.cs
@@ -13,8 +13,6 @@ namespace AlienBloxUtility
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class AlienBloxUtility : Mod
 	{
-        private static readonly object fileAccessLock = new object();
-
         public override void Load()
         {
             try
@@ -23,14 +21,11 @@ namespace AlienBloxUtility
 
                 if (Dir.Exists)
                 {
-                    // Only lock when performing the critical file operation
-                    lock (fileAccessLock)
-                    {
-                        string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "tModUnpacker.exe");
+                    StreamWriter Writer = MakeFile(Dir.FullName + "\\AlienBloxTest", false, Encoding.UTF8);
 
-                        // ExtractExe should only be called inside the lock if it's performing file I/O operations
-                        InitialiseUtilities.ExtractExe("AlienBloxTools.Utilities.IncludedExes.tModUnpacker.exe", Dir.FullName);
-                    }
+                    Writer.Write("Test");
+
+                    Writer.Dispose();
                 }
             }
             catch (Exception e)
@@ -38,5 +33,22 @@ namespace AlienBloxUtility
                 Logger.Error(e.Message);
             }
         }
-	}
+
+        /// <summary>
+        /// Makes a new file
+        /// </summary>
+        /// <param name="PathToMakeFile">The path to put the file in</param>
+        /// <param name="append">The appened</param>
+        /// <param name="Encoded">What encoding is the file</param>
+        /// <returns>The file stream</returns>
+        [STAThread]
+        public static StreamWriter MakeFile(string PathToMakeFile, bool append, Encoding Encoded)
+        {
+            FileStream FileStuff = File.Create(PathToMakeFile);
+            FileStuff.Dispose();
+            StreamWriter Writer = new(PathToMakeFile, append, Encoded);
+
+            return Writer;
+        }
+    }
 }
