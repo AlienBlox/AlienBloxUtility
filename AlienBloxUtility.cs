f23b815e60867460e43ab5fe3a6fc74961046419
AlienBlox <alienthebest123@outlook.com>
2025-12-22 12:51:35 +0000

    lua fixes

--- AlienBloxUtility.cs
+++ AlienBloxUtility.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.IO;
 using Terraria.ModLoader;
-
+    
 namespace AlienBloxUtility
 {
     // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
