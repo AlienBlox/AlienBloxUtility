415b683c116acb636d4bfbc0e7acc5853a8d791d
AlienBlox <alienthebest123@outlook.com>
2025-12-26 18:48:10 +0000

    add tools

---
+++ Utilities/Commands/MovieCreditsCommand.cs
@@ -0,0 +1,18 @@
+﻿using AlienBloxUtility.Utilities.Abstracts;
+using AlienBloxUtility.Utilities.UIUtilities.UIStates;
+using Terraria.Localization;
+
+namespace AlienBloxUtility.Utilities.Commands
+{
+    public class MovieCreditsCommand : CommandBase
+    {
+        public override string CommandName => "credits";
+
+        public override void LaunchCommand(ConHostSystem Conhost, params string[] Params)
+        {
+            Conhost.AddConsoleText(Language.GetText("Mods.AlienBloxUtility.Messages.ConsoleCredits.AlienBloxCredit").Value);
+            Conhost.AddConsoleText(Language.GetText("Mods.AlienBloxUtility.Messages.ConsoleCredits.Testers").Value);
+            Conhost.AddConsoleText(Language.GetText("Mods.AlienBloxUtility.Messages.ConsoleCredits.Libraries").Value);
+        }
+    }
+}
