f9ef82b4f96cc21a91c93d981d2e87c52688eeb1
AlienBlox <alienthebest123@outlook.com>
2025-12-27 15:37:05 +0000

    upd

--- Utilities/Core/UtilityPlayer.cs
+++ Utilities/Core/UtilityPlayer.cs
@@ -1,29 +1,13 @@
-﻿using Terraria;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 
 namespace AlienBloxUtility.Utilities.Core
 {
     public class UtilityPlayer : ModPlayer
     {
-        public int updateTimer;
-
         public override void OnEnterWorld()
         {
             AlienBloxUtility.SendSteamID(Player);
             //AlienBloxUtility.RetrieveSteamID();
-        }
-
-        public override void PostUpdateMiscEffects()
-        {
-            if (Main.myPlayer == Player.whoAmI)
-            {
-                if (updateTimer++ >= 30)
-                {
-                    AlienBloxUtility.RetrieveSteamID();
-
-                    updateTimer = 0; 
-                }
-            }
         }
     }
 }
