45b99d5c25344abf700f25281c2ab44612696715
AlienBlox <alienthebest123@outlook.com>
2026-01-18 14:55:25 +0000

    scare

--- Utilities/UIUtilities/UIElements/HorrificScream.cs
+++ Utilities/UIUtilities/UIElements/HorrificScream.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AlienBloxUtility.Utilities.Helpers;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace AlienBloxUtility.Utilities.UIUtilities.UIElements
 {
-    internal class HorrificScream
+    public class HorrificScream : UIElement
     {
+        public Texture2D texture;
+
+        public HorrificScream()
+        {
+            texture = ModContent.Request<Texture2D>("AlienBloxUtility/Dont_click_and_see_this_please.").Value;
+
+            Width.Set(1430, 0);
+            Height.Set(1080, 0);
+            VAlign = HAlign = .5f;
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, GetDimensions().Position(), new(255, 255, 255));
+
+            base.DrawSelf(spriteBatch);
+        }
+
+        public override void LeftClick(UIMouseEvent evt)
+        {
+            ScreamJumpscare.ScreamVisual = false;
+
+            base.LeftClick(evt);
+        }
     }
 }
---
+++ Utilities/UIUtilities/UIStates/Scare.cs
@@ -0,0 +1,15 @@
+﻿using AlienBloxUtility.Utilities.UIUtilities.UIElements;
+using Terraria.UI;
+
+namespace AlienBloxUtility.Utilities.UIUtilities.UIStates
+{
+    public class Scare : UIState
+    {
+        public override void OnInitialize()
+        {
+            HorrificScream scream = new();
+
+            Append(scream);
+        }
+    }
+}
