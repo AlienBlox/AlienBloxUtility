bf8a2ab5d875e7cdfb3180b6da72bf8b67360af0
AlienBlox <alienthebest123@outlook.com>
2026-01-19 11:32:38 +0000

    a

---
+++ Utilities/Helpers/WorldfileSaver.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+
+namespace AlienBloxUtility.Utilities.Helpers
+{
+    public static class WorldfileSaver
+    {
+        public static void SaveWorldFile()
+        {
+            var world = Main.ActiveWorldFileData;
+
+            if (!world.IsCloudSave)
+                world.CopyToLocal(world.Name + " (Autodump)");
+            else
+            {
+                world.MoveToLocal();
+                world.CopyToLocal(world.Name + " (Autodump)");
+                world.MoveToCloud();
+            }
+        }
+    }
+}
