df6805126151d09c52264c5a50dd44f542a68cda
AlienBlox <alienthebest123@outlook.com>
2026-01-21 22:22:13 +0000

    lua

---
+++ Utilities/DataStructures/LuaModule.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace AlienBloxUtility.Utilities.DataStructures
+{
+    public class LuaModule : ILoadable
+    {
+        public static List<LuaModule> AllModules;
+
+        public string Name => GetType().Name;
+
+        public Mod Mod {  get; private set; }
+
+        public virtual void OnLoad()
+        {
+
+        }
+
+        public virtual void OnUnload()
+        {
+
+        }
+
+        public void Load(Mod mod)
+        {
+            AllModules ??= [];
+            Mod = mod;
+
+            OnLoad();
+
+            AllModules.Add(this);
+        }
+
+        public void Unload()
+        {
+            OnUnload();
+
+            AllModules?.Remove(this);
+        }
+    }
+}
