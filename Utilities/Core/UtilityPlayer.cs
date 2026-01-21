2cea72ff222b8f0802569c42da1777c9653cc68b
AlienBlox <alienthebest123@outlook.com>
2026-01-21 15:07:53 +0000

    wee

--- Utilities/Core/UtilityPlayer.cs
+++ Utilities/Core/UtilityPlayer.cs
@@ -128,7 +128,7 @@ namespace AlienBloxUtility.Utilities.Core
         public override void OnEnterWorld()
         {
             noClipHack = false;
-            AlienBloxUtility.SendNoclipHack(new Vector2(Main.spawnTileX * 16, Main.spawnTileY * 16), noClipHack);
+            AlienBloxUtility.SendNoclipHack(Player.position, noClipHack);
 
             //DebugSidebarRender.Instance.RegenUI();
             AlienBloxUtility.SendSteamID(Player);
