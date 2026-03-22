fb3d4b6462f06dbcfeb9157a5aa15cb190416e1b
AlienBlox <alienthebest123@outlook.com>
2026-03-22 12:23:20 +0000

    e

--- Utilities/Helpers/ItemEnable.cs
+++ Utilities/Helpers/ItemEnable.cs
@@ -5,45 +5,28 @@ namespace AlienBloxUtility.Utilities.Helpers
 {
     public class ItemEnable : GlobalItem
     {
+        internal static bool[] ItemsThatShouldNotBeInInventory;
+
+        internal static bool[] Deprecated;
+
         public override void SetStaticDefaults()
         {
-            if (!AlienBloxUtilityServerConfig.Instance.EnableBrokenItems)
-                return;
+            bool instance = AlienBloxUtilityServerConfig.Instance.EnableBrokenItems;
 
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.FirstFractal] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.SkeletonBow] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.OgreMask] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.ApplePieSlice] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.KoboldDynamiteBackpack] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.EtherianJavelin] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.GoblinBomberCap] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.GoblinMask] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.PhasicWarpEjector] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.VortexAxe] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.StardustAxe] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.NebulaAxe] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.SolarFlareAxe] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.VortexChainsaw] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.NebulaChainsaw] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.SolarFlareChainsaw] = false;
-            ItemID.Sets.ItemsThatShouldNotBeInInventory[ItemID.StardustChainsaw] = false;
-            ItemID.Sets.Deprecated[ItemID.FirstFractal] = false;
-            ItemID.Sets.Deprecated[ItemID.SkeletonBow] = false;
-            ItemID.Sets.Deprecated[ItemID.OgreMask] = false;
-            ItemID.Sets.Deprecated[ItemID.ApplePieSlice] = false;
-            ItemID.Sets.Deprecated[ItemID.KoboldDynamiteBackpack] = false;
-            ItemID.Sets.Deprecated[ItemID.EtherianJavelin] = false;
-            ItemID.Sets.Deprecated[ItemID.GoblinBomberCap] = false;
-            ItemID.Sets.Deprecated[ItemID.GoblinMask] = false;
-            ItemID.Sets.Deprecated[ItemID.PhasicWarpEjector] = false;
-            ItemID.Sets.Deprecated[ItemID.VortexAxe] = false;
-            ItemID.Sets.Deprecated[ItemID.StardustAxe] = false;
-            ItemID.Sets.Deprecated[ItemID.NebulaAxe] = false;
-            ItemID.Sets.Deprecated[ItemID.SolarFlareAxe] = false;
-            ItemID.Sets.Deprecated[ItemID.VortexChainsaw] = false;
-            ItemID.Sets.Deprecated[ItemID.NebulaChainsaw] = false;
-            ItemID.Sets.Deprecated[ItemID.SolarFlareChainsaw] = false;
-            ItemID.Sets.Deprecated[ItemID.StardustChainsaw] = false;
+            ItemsThatShouldNotBeInInventory = (bool[])ItemID.Sets.ItemsThatShouldNotBeInInventory.Clone();
+            Deprecated = (bool[])ItemID.Sets.Deprecated.Clone();
+
+            for (int i = 0; i < ItemID.Sets.ItemsThatShouldNotBeInInventory.Length; i++)
+            {
+                if (ItemID.Sets.ItemsThatShouldNotBeInInventory[i])
+                    ItemID.Sets.ItemsThatShouldNotBeInInventory[i] = !instance;
+            }
+
+            for (int i = 0; i < ItemID.Sets.Deprecated.Length; i++)
+            {
+                if (ItemID.Sets.ItemsThatShouldNotBeInInventory[i])
+                    ItemID.Sets.Deprecated[i] = !instance;
+            }
         }
     }
 }
---
+++ Utilities/UtilityUnloader.cs
@@ -0,0 +1,15 @@
+﻿using AlienBloxUtility.Utilities.Helpers;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AlienBloxUtility.Utilities
+{
+    public class UtilityUnloader : ModSystem
+    {
+        public override void OnModUnload()
+        {
+            ItemID.Sets.ItemsThatShouldNotBeInInventory = ItemEnable.ItemsThatShouldNotBeInInventory;
+            ItemID.Sets.Deprecated = ItemEnable.Deprecated;
+        }
+    }
+}
