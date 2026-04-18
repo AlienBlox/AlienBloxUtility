990e31809e8ef5b49019ab2599e16651dbde54d3
AlienBlox <alienthebest123@outlook.com>
2026-04-18 15:13:34 +0100

    e

---
+++ Utilities/UIUtilities/UIStates/UltimateAudioInterface.cs
@@ -0,0 +1,22 @@
+﻿using AlienBloxUtility.Utilities.UIUtilities.UIElements;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
+using Terraria.UI;
+
+namespace AlienBloxUtility.Utilities.UIUtilities.UIStates
+{
+    public class UltimateAudioInterface : UIState
+    {
+        public PanelV2 MainPanel;
+
+        public UIPanel MusicPlayerBG, Nameplate, DownloadButton;
+
+        public override void OnInitialize()
+        {
+            MainPanel = new(new(300, 150), new(0, 0), new(200, 150, 0), Color.Black, Language.GetText("Mods.AlienBloxUtility.UI.MusicTool").Value, true, false, 650, 450);
+
+            Append(MainPanel);
+        }
+    }
+}
